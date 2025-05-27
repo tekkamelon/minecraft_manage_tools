b280704e040da7ff982c0e58e06a8c8a1eacf7b3
tekkamelon <type2guntank@gmail.com>
2025-05-28 01:44:46 +0900

    "mc_stop.sh"コマンドを追加
    サーバー停止用スクリプトを追加

---
+++ bin/mc_stop.sh
@@ -0,0 +1,23 @@
+#!/bin/sh 
+
+set -eu
+set -xv
+
+# セッション名
+session_name="minecraft"
+
+if tmux has-session -t "${session_name}" 2>/dev/null; then
+
+    # セッションが存在する場合の処理
+	# マインクラフトサーバーに"stop"コマンドを送る
+    tmux send-keys -t "${session_name}" "stop" C-m
+
+else
+
+    # セッションが存在しない場合の処理
+    echo "セッション '${session_name}' が見つかりません。" 1>&2
+
+    exit 1
+
+fi
+
