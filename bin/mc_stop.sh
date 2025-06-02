e3916ba050fff6a85221e6a1fde2f54de389c3f4
tekkamelon <type2guntank@gmail.com>
2025-06-03 00:05:53 +0900

    軽微な修正
    マイクラのプロセス判断の処理を修正

--- bin/mc_stop.sh
+++ bin/mc_stop.sh
@@ -1,13 +1,18 @@
 #!/bin/sh 
 
-set -eu
+set -u
 # set -xv
 
+# マイクラサーバーのプロセスを取得
+mc_proc=$(pgrep -f "java.*server.jar")
 # ログイン中のプレイヤー
 player=$(rcon-cli "list" | awk -F ': ' '{print $2}')
 
+set -u
+
 # マイクラサーバーが停止していれば真
-if ! pgrep -f "java.*server.jar" > /dev/null; then
+# if ! pgrep -f "java.*server.jar" > /dev/null; then
+if [ -z "${mc_proc}" ]; then
 
 	echo "マインクラフトサーバーは既に停止しています" 1>&2
 
