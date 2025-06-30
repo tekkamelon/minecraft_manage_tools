b6eff3cf4ce2f1571f54db8a7e6562666eeb2e7c
tekkamelon <type2guntank@gmail.com>
2025-06-30 21:57:14 +0900

    分岐の処理を追加
    マイクラサーバーのプロセスの有無を確認する処理を追加

--- bin/mc_status.sh
+++ bin/mc_status.sh
@@ -1,28 +1,42 @@
 #!/bin/sh 
 
-set -eu
+set -u
 
-# === 変数の定義 ===
-# ログイン中のプレイヤー
-player=$(rcon-cli "list")
-# ログからバージョンを取得
-version=$(grep < "${HOME}/Minecraft/logs/latest.log" - F "version" | cut -d ' ' -f7-)
-# シード値
-seed=$(rcon "seed")
-# === 変数の定義ここまで ===
+# マイクラサーバーのプロセスを取得
+mc_proc=$(pgrep -f "java.*server.jar")
+
+set -e
+
+# マイクラサーバーが起動していれば真
+if [ -n "${mc_proc}" ]; then
+
+	# ログイン中のプレイヤー
+	player=$(rcon-cli "list")
+
+	# ログからバージョンを取得
+	version=$(grep < "${HOME}/Minecraft/logs/latest.log" -F "version" | cut -d ' ' -f7-)
+	# シード値
+	seed=$(rcon-cli "seed")
 
 
-# 出力
-cat <<EOF
-# === Minecraft Server Status ===
+	# 出力
+	cat <<- EOF
+	# === Minecraft Server Status ===
 
-### Version 
-${version}
+	### Version 
+	${version}
 
-### Players 
-${player}
+	### Players 
+	${player}
 
-### Seed
-${seed}
-EOF
+	### Seed
+	${seed}
+	EOF
 
+else
+
+	# エラーメッセージを出力
+	echo "Server is Down!" 1>&2
+	exit 1
+
+fi
