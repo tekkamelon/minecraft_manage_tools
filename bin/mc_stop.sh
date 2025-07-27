54da26deae83c080e44fbc6fbc3a2b90c66905bf
tekkamelon <type2guntank@gmail.com>
2025-07-28 02:04:19 +0900

    サーバー停止中に実行された場合の処理を修正
    エラーメッセージにrcon-cliのエラーが入るバグを修正

--- bin/mc_stop.sh
+++ bin/mc_stop.sh
@@ -5,17 +5,19 @@ set -eu
 # マイクラサーバーのプロセスを取得
 mc_pid="$(pgrep -f "java.*server.jar" || true)"
 
-# ログイン中のプレイヤー
-logged_in_players="$(rcon-cli "list" | awk -F ': ' '{print $2}' || true)"
-
 # マイクラサーバーが停止していれば真
 if [ -z "${mc_pid}" ]; then
 
 	echo "マインクラフトサーバーは既に停止しています" 1>&2
 	exit 1
 
+fi
+
+# ログイン中のプレイヤー
+logged_in_players="$(rcon-cli "list" | awk -F ': ' '{print $2}' || true)"
+
 # ログインしているプレイヤーがいない場合
-elif [ -n "${logged_in_players}" ]; then
+if [ -n "${logged_in_players}" ]; then
 
 	# エラーメッセージとプレイヤーを出力
 	{ echo "サーバーにログイン中のプレイヤーがいます" ; rcon-cli "list" ; } 1>&2
